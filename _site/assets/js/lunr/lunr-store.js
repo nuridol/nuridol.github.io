dece61770d18776e497c8f2a1fc787fe9d0e3d81
NuRi <nuridol+github@gmail.com>
2020-12-04 23:30:35 +0900

    Add first post
    블로그 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "새 블로그",
+        "excerpt":"github.io 블로그로 개발 내용을 간단히 정리해 보려고 합니다.   ","categories": ["blog"],
+        "tags": ["blog"],
+        "url": "https://www.nuridol.net/blog/first-post/",
+        "teaser": null
+      }]
