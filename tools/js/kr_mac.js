3a142b43e25a71eb2665fdd3795da9a8e0cf9046
NuRi <nuridol+github@gmail.com>
2020-12-18 11:30:44 +0900

    맥북 재고 확인 추가
    - M1칩 모델 추가

---
+++ tools/js/kr_mac.js
@@ -0,0 +1,15 @@
+// M1 models (2020.12.18)
+// https://www.apple.com/kr/shop/buy-mac/macbook-air
+var MODEL_LIST = {
+  'MGN63KH/A':'M1 Air Gray/256GB',
+  'MGND3KH/A':'M1 Air Gold/256GB',
+  'MGN93KH/A':'M1 Air Silver/256GB',
+  'MGN73KH/A':'M1 Air Gray/512GB',
+  'MGNE3KH/A':'M1 Air Gold/512GB',
+  'MGNA3KH/A':'M1 Air Silver/512GB',
+
+  'MYD82KH/A':'M1 Pro Gray/256GB',
+  'MYDA2KH/A':'M1 Pro Silver/256GB',
+  'MYD92KH/A':'M1 Pro Gray/512GB',
+  'MYDC2KH/A':'M1 Pro Silver/512GB'
+};
