5f0fa026fcedb6d0beff3259e765f3fc04689b7b
NuRi <nuridol+github@gmail.com>
2021-06-04 15:34:24 +0900

    auto commit 2021-06-04-15:34:24

---
+++ tools/js/kr_setup.js
@@ -0,0 +1,34 @@
+$(function() {
+    var itemType = "";
+    function setupItemType() {
+        if (!$('#options').length) {
+            console.log("no input");
+            return;
+        }
+        // check url parameter
+        var currentUrl = window.location.href;
+        var urlMatch = currentUrl.match(/html#([0-9a-z]*)/);
+        var type = "ipad";
+        if (urlMatch && urlMatch.length > 0) {
+            type = urlMatch[1];
+        }
+        var optionId = '#option_' + type;
+        if ($(optionId)) {
+            $(optionId).prop('class', 'btn btn-success active');
+            // $(optionId).button('toggle');
+            itemType = type;
+        }
+        $("#options a").click(function () {
+            setTimeout("location.reload(true);", 2);
+        });
+        // load model list
+        let script1 = document.createElement('script');
+        script1.src = "js/kr_" + itemType + ".js";
+        let script2 = document.createElement('script');
+        script2.src = "js/kr_stocks.js";
+        document.body.append(script1);
+        document.body.append(script2);
+    }
+
+    setupItemType();
+});
