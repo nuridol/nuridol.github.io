5650368a7de953345b16fb1493a096dd9f8e446c
NuRi <nuridol+github@gmail.com>
2021-09-23 19:07:37 +0900

    auto commit 2021-09-23-19:07:36

---
+++ tools/js/jp_ipad.js
@@ -0,0 +1,28 @@
+// pad & mini models (2021.09.15)
+var MODEL_LIST = {
+'MK2K3J/A': 'ipad 64GB space gray',
+'MK2N3J/A': 'ipad 256GB space gray',
+'MK2L3J/A': 'ipad 64GB silver',
+'MK2P3J/A': 'ipad 256GB silver',
+'MK473J/A': 'ipad 64GB space gray',
+'MK4E3J/A': 'ipad 256GB space gray',
+'MK493J/A': 'ipad 64GB silver',
+'MK4H3J/A': 'ipad 256GB silver',
+
+'MK7R3J/A': 'mini 64GB purple',
+'MLWL3J/A': 'mini 64GB pink',
+'MK7P3J/A': 'mini 64GB starlight',
+'MK7M3J/A': 'mini 64GB space gray',
+'MK7X3J/A': 'mini 256GB purple',
+'MLWR3J/A': 'mini 256GB pink',
+'MK7V3J/A': 'mini 256GB starlight',
+'MK7T3J/A': 'mini 256GB space gray',
+'MK8E3J/A': 'mini 64GB purple',
+'MLX43J/A': 'mini 64GB pink',
+'MK8C3J/A': 'mini 64GB starlight',
+'MK893J/A': 'mini 64GB space gray',
+'MK8K3J/A': 'mini 256GB purple',
+'MLX93J/A': 'mini 256GB pink',
+'MK8H3J/A': 'mini 256GB starlight',
+'MK8F3J/A': 'mini 256GB space gray'
+};
