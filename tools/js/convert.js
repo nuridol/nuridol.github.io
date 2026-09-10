b80924a5448e3589f18c52a138add9338ae7c1eb
Suhwan Seo <nuridol@gmail.com>
2026-09-10 18:13:28 +0900

    refactor(tools): update iframe and youtube converters to Bootstrap 5 and Vanilla JS

--- tools/js/convert.js
+++ tools/js/convert.js
@@ -1,75 +1,97 @@
-
+// NuRi's YouTube iframe to embed/object tag converter (Vanilla JS)
 (function () {
-    if (typeof jQuery == 'undefined') {
-        return;
+    'use strict';
+
+    function convertYoutubeCode() {
+        const codeInput = document.getElementById('code');
+        const resultArea = document.getElementById('result');
+        const copyBtn = document.getElementById('btnCopy');
+        if (!codeInput || !resultArea) return;
+
+        let code = codeInput.value.trim();
+        let convertedCode = '';
+
+        if (code.match(/^<iframe/i) && (code.includes('youtube.com') || code.includes('youtube-nocookie.com'))) {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(code, 'text/html');
+            const iframe = doc.querySelector('iframe');
+
+            if (iframe) {
+                let width = iframe.getAttribute('width');
+                if (parseInt(width, 10) > 0) {
+                    width = parseInt(width, 10);
+                } else {
+                    width = '100%';
+                }
+
+                let height = iframe.getAttribute('height');
+                if (parseInt(height, 10) > 0) {
+                    height = parseInt(height, 10);
+                } else {
+                    height = 'auto';
+                }
+
+                let fullScreen = 'false';
+                if (code.includes('allowfullscreen')) {
+                    fullScreen = 'true';
+                }
+
+                let videoSrc = (iframe.getAttribute('src') || '')
+                    .replace('www.youtube.com/embed', 'www.youtube.com/v')
+                    .replace('www.youtube-nocookie.com/embed', 'www.youtube-nocookie.com/v');
+
+                if (videoSrc.includes('?')) {
+                    videoSrc = videoSrc + '&version=3';
+                } else {
+                    videoSrc = videoSrc + '?version=3';
+                }
+
+                const convertedVideoCode = '<div><object width="' + width + '" height="' + height + '"><param name="movie" value="' + videoSrc + '"></param><param name="allowFullScreen" value="' + fullScreen + '"></param><param name="allowscriptaccess" value="always"></param><embed src="' + videoSrc + '" type="application/x-shockwave-flash" width="' + width + '" height="' + height + '" allowscriptaccess="always" allowfullscreen="' + fullScreen + '"></embed></object></div>';
+
+                convertedCode = convertedVideoCode + '<p><a href="https://nuridol.net/ut_convert.html">NuRi\'s Tools - YouTube 변환기</a></p>';
+            }
+        }
+
+        resultArea.value = convertedCode;
+        resultArea.removeAttribute('disabled');
+        if (copyBtn) {
+            copyBtn.disabled = !convertedCode;
+        }
     }
-    jQuery.noConflict();
-    var $ = jQuery;
 
-function convertYoutubeCode() {
-    var code = $("#code").val().replace(/^\s+|\s+$/, "");
-    var convertedCode = "";
+    function copyResult() {
+        const resultArea = document.getElementById('result');
+        const copyBtn = document.getElementById('btnCopy');
+        if (!resultArea || !resultArea.value) return;
 
-    if  (code.match(/^<iframe width=/) && (code.match("www.youtube.com") || code.match("www.youtube-nocookie.com"))) {
-        $('#testbox').css('display', 'none');
-
-        $("#testbox").html(code);
-        var width  = $("#testbox").find('iframe').attr('width');
-        if (parseInt(width, 10) > 0) {
-            width = parseInt(width, 10); //+ 'px';
-        }
-        else {
-            width = "100%";
-        }
-
-        var height = $("#testbox").find('iframe').attr('height');
-        if (parseInt(height, 10) > 0) {
-            height = parseInt(height, 10); // + 'px';
-        }
-        else {
-            height = "auto";
-        }
-
-        var fullScreen = "false";
-        if (code.match("allowfullscreen")) {
-            fullScreen = "true";
-        }
-
-        var videoSrc = $("#testbox").find('iframe').attr('src').replace("www.youtube.com/embed", "www.youtube.com/v");
-        videoSrc = videoSrc.replace("www.youtube-nocookie.com/embed", "www.youtube-nocookie.com/v");
-
-        if (videoSrc.match("\\?")) {
-            videoSrc = videoSrc + "&version=3";
-        }
-        else {
-            videoSrc = videoSrc + "?version=3";
-            //remove tag: &hl=ko_KR
-        }
-
-        var convertedVideoCode = '<div><object width="' + width + '" height="' + height + '"><param name="movie" value="' + videoSrc + '"></param><param name="allowFullScreen" value="' + fullScreen + '"></param><param name="allowscriptaccess" value="always"></param><embed src="' + videoSrc + '" type="application/x-shockwave-flash" width="' + width + '" height="' + height + '" allowscriptaccess="always" allowfullscreen="' + fullScreen + '"></embed></object></div>';
-
-        convertedCode = convertedVideoCode + '<p><a href="https://nuridol.net/ut_convert.html">NuRi\'s Tools - YouTube 변환기</a></p>';
-
-        $("#testbox").html("");
-
-        $('#resultbox').css('display', 'block');
-    }
-
-    $("#result").text(convertedCode);
-    $("#result").removeAttr("disabled");
-}
-
-
-    $(document).ready(function () {
-        if (!$('#code') || !$('#result') || !$('#convert')) {
-            return;
-        }
-
-        $('#convert').click(function () {
-            convertYoutubeCode();
+        navigator.clipboard.writeText(resultArea.value).then(() => {
+            if (copyBtn) {
+                const originalHtml = copyBtn.innerHTML;
+                copyBtn.innerHTML = '<i class="bi bi-check-lg me-1"></i> 복사됨!';
+                copyBtn.classList.remove('btn-outline-secondary');
+                copyBtn.classList.add('btn-success');
+                setTimeout(() => {
+                    copyBtn.innerHTML = originalHtml;
+                    copyBtn.classList.remove('btn-success');
+                    copyBtn.classList.add('btn-outline-secondary');
+                }, 2000);
+            }
+        }).catch(err => {
+            console.error('Copy failed:', err);
+            resultArea.select();
+            document.execCommand('copy');
         });
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const convertBtn = document.getElementById('convert');
+        if (convertBtn) {
+            convertBtn.addEventListener('click', convertYoutubeCode);
+        }
+
+        const copyBtn = document.getElementById('btnCopy');
+        if (copyBtn) {
+            copyBtn.addEventListener('click', copyResult);
+        }
     });
-
-
 })();
-
--- tools/js/if_convert.js
+++ tools/js/if_convert.js
@@ -1,72 +1,96 @@
-
+// NuRi's iframe to object tag converter (Vanilla JS)
 (function () {
-    if (typeof jQuery == 'undefined') {
-        return;
+    'use strict';
+
+    function convertCode() {
+        const codeInput = document.getElementById('code');
+        const resultArea = document.getElementById('result');
+        const copyBtn = document.getElementById('btnCopy');
+        if (!codeInput || !resultArea) return;
+
+        let code = codeInput.value.replace(/\n/g, '').trim();
+        let convertedCode = '';
+
+        if (code.match(/^<iframe/i)) {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(code, 'text/html');
+            const iframe = doc.querySelector('iframe');
+
+            if (iframe) {
+                let width = iframe.getAttribute('width');
+                if (parseInt(width, 10) > 0) {
+                    width = parseInt(width, 10);
+                } else {
+                    width = '100%';
+                }
+
+                let height = iframe.getAttribute('height');
+                if (parseInt(height, 10) > 0) {
+                    height = parseInt(height, 10);
+                } else {
+                    height = 'auto';
+                }
+
+                let extraAttrs = '';
+                for (let i = 0; i < iframe.attributes.length; i++) {
+                    const attr = iframe.attributes[i];
+                    if (attr.name === 'src' || attr.name === 'width' || attr.name === 'height') {
+                        continue;
+                    }
+                    extraAttrs += ' ' + attr.name;
+                    if (attr.value !== '') {
+                        extraAttrs += '="' + attr.value + '"';
+                    }
+                }
+
+                const iframeSrc = iframe.getAttribute('src') || '';
+                const extraCode = code.replace(/<iframe[\s\S]*?<\/iframe>/i, '');
+
+                convertedCode = '<div><object width="' + width + '" height="' + height + '" data="' + iframeSrc + '" type="text/html"' + extraAttrs + '></object></div>';
+                convertedCode = convertedCode + extraCode + '<p><a href="https://nuridol.net/if_convert.html">NuRi\'s Tools - iframe 변환기</a></p>';
+            }
+        }
+
+        resultArea.value = convertedCode;
+        resultArea.removeAttribute('disabled');
+        if (copyBtn) {
+            copyBtn.disabled = !convertedCode;
+        }
     }
-    jQuery.noConflict();
-    var $ = jQuery;
 
-  function convertCode() {
-    var code = $("#code").val().replace(/\n/g, "");
-    code = code.replace(/^\s+|\s+$/, "");
+    function copyResult() {
+        const resultArea = document.getElementById('result');
+        const copyBtn = document.getElementById('btnCopy');
+        if (!resultArea || !resultArea.value) return;
 
-    var convertedCode = "";
-    if  (code.match(/^<iframe/)) {
-
-//        $('#testbox').css('display', 'block');
-        $('#testbox').css('display', 'none');
-
-        $("#testbox").html(code);
-        var width  = $("#testbox").find('iframe:first').attr('width');
-        if (parseInt(width, 10) > 0) {
-            width = parseInt(width, 10);
-        }
-        else {
-            width = "100%";
-        }
-
-        var height = $("#testbox").find('iframe:first').attr('height');
-        if (parseInt(height, 10) > 0) {
-            height = parseInt(height, 10);
-        }
-        else {
-            height = "auto";
-        }
-
-        var extraCode = code.replace(/<iframe.*<\/iframe>/, "");
-
-        var extraAttrs = "";
-        var attrs = $("#testbox").find('iframe:first')[0].attributes;
-        for(var i=0;i<attrs.length;i++) {
-          if (attrs[i].nodeName == "src" || attrs[i].nodeName == "width" || attrs[i].nodeName == "height") {
-            continue;
-          }
-          extraAttrs = extraAttrs + " " + attrs[i].nodeName;
-          if (attrs[i].nodeValue != "") {
-            extraAttrs = extraAttrs + '="' + attrs[i].nodeValue + '"';
-          }
-        }
-
-        var iframeSrc = $("#testbox").find('iframe:first').attr('src');
-        convertedCode = '<div><object width="' + width + '" height="' + height + '" data="' + iframeSrc + '" type="text/html" ' + extraAttrs + '></object></div>';
-        convertedCode = convertedCode + extraCode + '<p><a href="https://nuridol.net/if_convert.html">NuRi\'s Tools - iframe 변환기</a><p>';
-
-        $("#testbox").html("");
-    }
-
-    $("#result").text(convertedCode);
-    $("#result").removeAttr("disabled");
-}
-
-    $(document).ready(function () {
-        if (!$('#code') || !$('#result') || !$('#convert')) {
-            return;
-        }
-
-        $('#convert').click(function () {
-            convertCode();
+        navigator.clipboard.writeText(resultArea.value).then(() => {
+            if (copyBtn) {
+                const originalHtml = copyBtn.innerHTML;
+                copyBtn.innerHTML = '<i class="bi bi-check-lg me-1"></i> 복사됨!';
+                copyBtn.classList.remove('btn-outline-secondary');
+                copyBtn.classList.add('btn-success');
+                setTimeout(() => {
+                    copyBtn.innerHTML = originalHtml;
+                    copyBtn.classList.remove('btn-success');
+                    copyBtn.classList.add('btn-outline-secondary');
+                }, 2000);
+            }
+        }).catch(err => {
+            console.error('Copy failed:', err);
+            resultArea.select();
+            document.execCommand('copy');
         });
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const convertBtn = document.getElementById('convert');
+        if (convertBtn) {
+            convertBtn.addEventListener('click', convertCode);
+        }
+
+        const copyBtn = document.getElementById('btnCopy');
+        if (copyBtn) {
+            copyBtn.addEventListener('click', copyResult);
+        }
     });
-
 })();
-
